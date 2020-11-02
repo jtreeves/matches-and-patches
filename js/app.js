a37f82d5bdf706d6846270fff0807e553835ef4c
jtreeves <jackson.t.reeves@gmail.com>
2020-11-02 16:23:57 -0500

    [js] Trying to make first four tiles display after clicking button

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+// Grabs patches and handles what to do when you click them
 const patches = document.querySelectorAll('.patch')
 const smallPatches = document.querySelectorAll('.small-patch')
 
@@ -7,4 +8,19 @@ for (let i = 0; i < patches.length; i++) {
 
 for (let i = 0; i < smallPatches.length; i++) {
     smallPatches[i].addEventListener('click', function(){smallPatches[i].innerText = 'clicked'})
+}
+
+// Grab buttons
+const play = document.querySelector('#play')
+play.addEventListener('click', dealTiles())
+
+// Grabs tiles
+const tiles = document.querySelectorAll('.tile')
+
+// Deals the tiles at the start of the game
+function dealTiles() {
+    tiles[0].style.display = '';
+    tiles[1].style.display = '';
+    tiles[2].style.display = '';
+    tiles[3].style.display = '';
 }
