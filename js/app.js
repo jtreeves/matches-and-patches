584ea8073ee66dbbaff193367dd5d3a13fde3a10
jtreeves <jackson.t.reeves@gmail.com>
2020-11-10 06:40:11 -0500

    [project] Clean up code

--- js/app.js
+++ js/app.js
@@ -14,19 +14,19 @@ guide.append(guideHeader)
 const guideBody = document.createElement('div')
 guide.append(guideBody)
 
-const introduction = document.createElement('div')
-introduction.textContent = 'Welcome to Matches and Patches! This is a simple matching game. If you want to skip straight to the game, just click the Play button at the bottom left. If you want to learn more about the rules of the game, just click the More Info button below.'
+const introduction = document.createElement('p')
+introduction.textContent = 'Welcome to Matches and Patches! This is a simple matching game. Think Uno crossed with Dominoes, but played on a mini Checkers board. If you want to skip straight to the game, just click the Play button at the bottom left. If you want to learn more about the rules of the game, just click the More Info button below.'
 
-const slide1 = document.createElement('div')
+const slide1 = document.createElement('p')
 slide1.textContent = 'The game is played on a board with four rows and four columns. At the beginning of the game, you and your unseen opponent will be dealt four tiles. Each tile features a color, a number, and a shape. In each round, you and your opponent will play one tile on one of the patches on the board. If your tile matches an adjacent tile on the board, then you can capture the patch you played and the patch with the matching tile.'
 
-const slide2 = document.createElement('div')
+const slide2 = document.createElement('p')
 slide2.textContent = 'After playing your tiles, you and your opponent will each be dealt an additional tile. For every patch on the board that you capture, you get a point. The game ends when all the patches on the board are covered (or when there are not enough left for another round), and the winner is the player with the most points.'
 
-const slide3 = document.createElement('div')
+const slide3 = document.createElement('p')
 slide3.textContent = 'But what is a match? If two tiles share at least one feature (shape, number, or color), then they match. For instance, the Red Two Circle matches with the Red One Square because they share a color. It also matches with the Blue Two Triangle because they share a number. However, it does not match with the Blue Three Triangle because they do not share any common feature.'
 
-const slide4 = document.createElement('div')
+const slide4 = document.createElement('p')
 slide4.textContent = 'Still a little confused about the game? The best way to fix that is to play it! Plus, throughout the game, you can turn to this guide for feedback about what is happening in each round and what the current score is.'
 
 const moreInfo = document.createElement('div')
